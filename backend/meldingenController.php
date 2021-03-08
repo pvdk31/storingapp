117d0689738b38a8f0a6bb13a6676e1f084263e8
pvdk31 <pvdk.7@outlook.com>
2021-03-08 14:31:54 +0100

    Update meldingenController.php

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -5,13 +5,22 @@ $attractie = $_POST['attractie'];
 $capaciteit = $_POST['capaciteit']; 
 $melder = $_POST['melder'];
 
-echo $attractie . " / " . $capaciteit . " / " . $melder;
+// echo $attractie . " / " . $capaciteit . " / " . $melder;
 
 //1. Verbinding
 require_once 'conn.php';
 
 //2. Query
+$query = 'INSERT INTO meldingen (attractie, capaciteit, melder)
+            VALUES(attractie,capaciteit,melder)';
 
 //3. Prepare
+$statement = $conn->prepare($query);
 
-//4. Execute
+//.4 Execute
+$statement->execute([
+    ":attractie"=>$attractie,
+    ":capaciteit"=>$capaciteit,
+    ":melder"=>$melder]);
+
+header("Location: ../meldingen/index.php?msg=Melding opgeslagen");
